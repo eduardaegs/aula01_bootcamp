c0f8eaa1c8a90f70e407dc7e222851bbc84c704a
Eduarda Andrade <andrasi.eduarda@gmail.com>
2025-11-23 10:49:17 -0300

    Primeiros passos

--- exemplo_00.py
+++ exemplo_00.py
@@ -1,2 +1,5 @@
 print('Hello, world!')
 
+# Por padrao, nao se pode somar um número com texto. mas se as variaveis forem convertidas para o mesmo tipo, a operacao sera possivel
+
+print("Eduarda" + " " + "Andrade")  # concatenacao de strings
