712209fe897d46b78aeec243abe641f0acfc5842
Eduarda Andrade <andrasi.eduarda@gmail.com>
2025-11-24 22:52:25 -0300

    Remove arquivo excluído (exemplo_00.py)

--- exemplo_00.py
+++
@@ -1,5 +0,0 @@
-print('Hello, world!')
-
-# Por padrao, nao se pode somar um número com texto. mas se as variaveis forem convertidas para o mesmo tipo, a operacao sera possivel
-
-print("Eduarda" + " " + "Andrade")  # concatenacao de strings
